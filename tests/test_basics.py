9f4265dad4b8547b06e058e7db5f35ed4c9daeb0
Hyukjae Kwark <hkwark.ai@gmail.com>
2023-11-25 21:08:36 -0800

    added unitttest

---
+++ tests/test_basics.py
@@ -0,0 +1,14 @@
+import unittest
+from unittest import TestCase
+
+
+class LinearTest(TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+
+    def test_forward(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
